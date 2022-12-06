eb2084843c3563f4c832bcc9b38209045459f43c
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-12-06 22:29:06 +0300

    add pages

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Footer from "../components/Footer";
+
+function Home ()
+{
+  return (
+    <div>
+      <Header />
+      <Search />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
+
