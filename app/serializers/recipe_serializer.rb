6299731ede002172fd2d18c6ea72a3dfc5353fb3
Alex_Wachira_Kimotho <alex.wachira@student.moringaschool.com>
2022-12-08 00:23:10 +0300

    Updated seializers and controllers

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,3 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :foodname, :ingredients, :servings, :country, :rating,  :description, :instructions, :image
 end
