1186e7a746980e4b8c641e94d983d6cf14a9faee
Joseph Njuguna <kaogire@gmail.com>
2022-12-11 15:47:24 +0300

    fix: update cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://recipes-fed.onrender.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
